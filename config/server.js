551ccae206f3dae21a325b97577d3630faa3da64
John Williams <jrwtango@gmail.com>
2020-10-13 09:57:11 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '20b80fc1a53932475c3d0c861917a638'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '07fd4373-e39b-45ef-b827-3360e659759b'
+};
